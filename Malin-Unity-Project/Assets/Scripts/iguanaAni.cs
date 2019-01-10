54196fa1e5fbc65d25c061437a2c0f75539f1e3d
Malin Niska <malni241@student.liu.se>
2019-01-10 22:35:54 +0100

    Done with page I-Q

---
+++ Malin-Unity-Project/Assets/Scripts/iguanaAni.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Vuforia;
+
+public class iguanaAni : MonoBehaviour, IVirtualButtonEventHandler
+{
+
+    public Text text;
+    public IguanaCharacter iguanaCharacter;
+
+    // Use this for initialization
+    void Start () {
+        iguanaCharacter = GetComponent<IguanaCharacter>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        //octopus.transform.Rotate(Vector3.right * Time.deltaTime);
+        //octopus.transform.RotateAround(octopus.transform.position, octopus.transform.up, Time.deltaTime * 10f);
+        iguanaCharacter.Attack();
+        text.text = "Should be playing animation...";
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+
+    }
+}
---
+++ Malin-Unity-Project/Assets/Scripts/playAnimation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Vuforia;
+
+public class playAnimation : MonoBehaviour, IVirtualButtonEventHandler
+{
+
+    public GameObject gameObj;
+    public Text text;
+
+    // Use this for initialization
+    void Start()
+    {
+        GameObject virtualButtonObject = this.gameObject;
+        virtualButtonObject.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        //octopus.transform.Rotate(Vector3.right * Time.deltaTime);
+        //octopus.transform.RotateAround(octopus.transform.position, octopus.transform.up, Time.deltaTime * 10f);
+        gameObj.GetComponent<Animation>().Play();
+        text.text = "Should be playing animation...";
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+
+    }
+}
